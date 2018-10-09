426d3491afa7cb752f5d5d34592944582cc2cf4a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-09 02:56:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   get '/square/:number' do
     @no = params[:number]
-    "#{Math.sqrt(@no.to_i).ceil}"
+    "#{@no.to_i * @no.to_i}"
   end
 
   get '/say/:number/:phrase' do
