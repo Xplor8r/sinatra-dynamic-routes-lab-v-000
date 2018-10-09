f421ed3e8f3b9373d4091d86388a0b520563e070
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-09 01:52:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,18 +7,19 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    
+    @no = params[:number]
+    "#{Math.sqrt(@no)}"
   end
 
   get '/say/:number/:phrase' do
-    
+
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
+
   end
 
   get '/:operation/:number1/:number2' do
-    
+
   end
 end
