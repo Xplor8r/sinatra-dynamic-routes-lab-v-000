1dad9b642d8ce364d50147808d101e50b2a6cdde
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-09 02:22:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,13 +12,12 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    phrase = ""
+    
     @no = params[:number]
     @phr = params[:phrase]
     @no.to_i.times do
-      phrase += @phr\n
+      @phr
     end
-    phrase
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
